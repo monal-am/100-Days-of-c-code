de81a709ef79851ea792590b41193c78cb67ec9d
monal-am <ambwanimonal@gmail.com>
2025-09-05 18:21:39 +0000

    Add files via upload

---
+++ sumof2num.c
@@ -0,0 +1,22 @@
+ #include <stdio.h>
+
+int main(void) {
+    int x, y, sum;
+    
+    // ask user for 2 numbers 
+    printf("Enter 1st number: ");
+    scanf("%d", &x);
+    
+    printf("Enter 2nd number: ");
+    scanf("%d", &y);
+     
+    // calculate the sum of two numbers 
+    sum = x + y;
+    
+    // display result 
+    printf("the sum = %d/n", sum);
+    
+    return 0;
+}
+
+    
